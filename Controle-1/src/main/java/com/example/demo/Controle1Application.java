ed88aebe537a97036c02f15eec19523aef0d2146
imadabdel2000 <imadabdel2000@gmail.com>
2022-04-25 16:52:47 +0200

    first commit

---
+++ Controle-1/src/main/java/com/example/demo/Controle1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controle1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Controle1Application.class, args);
+	}
+
+}
